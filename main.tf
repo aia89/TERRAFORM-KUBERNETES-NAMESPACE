936c452b5114f0501f84ff2da00a096426c2489b
aia89 <aia24dos@gmail.com>
2024-05-22 13:01:52 -0500

    created new module

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "kubernetes_namespace" "example" {
   metadata {
     name = var.name
+    annotations = var.annotations
+    labels = var.labels
   }
-}
+} 
+
+
 
 
--- variable.tf
+++ variable.tf
@@ -3,3 +3,15 @@ variable name {
   default     = ""
   description = "please provide a name"
 }
+
+variable annotations {
+  description = "Please provide annotations"
+  type = map
+  default = {}
+}
+
+variable labels {
+  description = "Please provide labels"
+  type = map
+  default = {}
+}
